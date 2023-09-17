8ba838e8772921eba1978b93b4c630148e5c7944
yanks-aky <y_autumn0901@icloud.com>
2023-09-17 22:59:26 +0900

    二部グラフ判定を追加

--- lib/graph.cpp
+++ lib/graph.cpp
@@ -246,6 +246,40 @@ bool is_dag(const vvll &edges) {
 
 #pragma endregion
 
+#pragma region "二部グラフ判定"
+
+class Bipartite {
+    const vvll &edges;
+
+    bool dfs(vll &colors, usll &vs, ll v, ll col) const {
+        vs.erase(v);
+        colors[v] = col;
+        repi(i, edges[v]) {
+            vs.erase(i);
+            if (colors[i] == col) return false;
+            if (colors[i] == 0 && !dfs(colors, vs, i, -col)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+public:
+    Bipartite(vvll &_edges) : edges(_edges) {}
+
+    bool is_bipartite() const {
+        vll colors(edges.size(), 0);
+        usll vs;
+        rep(i, edges.size()) vs.insert(i);
+        while (!vs.empty()) {
+            if (!dfs(colors, vs, *vs.begin(), 1)) return false;
+        }
+        return true;
+    }
+};
+
+#pragma endregion
+
 #pragma region "トポロジカルソート"
 
 vll topological_sort(const vvll &edges) {
