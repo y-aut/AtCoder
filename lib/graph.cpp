993b557496e4b9dad2077b9b33a89caff15e7300
yanks-aky <y_autumn0901@icloud.com>
2023-11-12 00:03:39 +0900

    重み付き Union-Find

--- lib/graph.cpp
+++ lib/graph.cpp
@@ -195,27 +195,80 @@ public:
 
 class UnionFind {
     const ll size;
-    vll parents;
+    vll par;
+    vll rank;
 
 public:
-    UnionFind(ll _size) : size(_size), parents(size, -1) {}
+    UnionFind(ll _size) : size(_size), par(size, -1), rank(size) {}
 
     ll root(ll v) {
-        if (parents[v] == -1) {
-            return v;
-        } else {
-            return parents[v] = root(parents[v]);
-        }
+        if (par[v] == -1) return v;
+        return par[v] = root(par[v]);
     }
 
-    bool is_connected(ll v1, ll v2) {
-        return root(v1) == root(v2);
+    bool same(ll x, ll y) {
+        return root(x) == root(y);
     }
 
-    void merge(ll v1, ll v2) {
-        if (is_connected(v1, v2))
-            return;
-        parents[root(v2)] = root(v1);
+    bool merge(ll x, ll y) {
+        x = root(x);
+        y = root(y);
+        if (x == y) return false;
+        if (rank[x] < rank[y]) swap(x, y);
+        if (rank[x] == rank[y]) ++rank[x];
+        par[y] = x;
+        return true;
+    }
+};
+
+#pragma endregion
+
+#pragma region "重み付き Union-Find"
+
+template <class Abel>
+class WUnionFind {
+    const ll size;
+    vll par;
+    vll rank;
+    vector<Abel> diff_weight;
+
+public:
+    // SUM_UNITY はアーベル群の単位元
+    WUnionFind(ll _size, Abel SUM_UNITY = 0) : size(_size), par(size, -1), rank(size),
+                                               diff_weight(size, SUM_UNITY) {}
+
+    ll root(ll v) {
+        if (par[v] == -1) return v;
+        auto r = root(par[v]);
+        diff_weight[v] += diff_weight[par[v]];
+        return par[v] = r;
+    }
+
+    Abel weight(ll v) {
+        root(v);
+        return diff_weight[v];
+    }
+
+    Abel diff(ll x, ll y) {
+        return weight(y) - weight(x);
+    }
+
+    bool same(ll x, ll y) {
+        return root(x) == root(y);
+    }
+
+    // weight(y) = weight(x) + w となるように merge する
+    bool merge(ll x, ll y, Abel w) {
+        w += weight(x);
+        w -= weight(y);
+        x = root(x);
+        y = root(y);
+        if (x == y) return false;
+        if (rank[x] < rank[y]) swap(x, y), w = -w;
+        if (rank[x] == rank[y]) ++rank[x];
+        par[y] = x;
+        diff_weight[y] = w;
+        return true;
     }
 };
 
