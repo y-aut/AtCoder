25a8296630b0c207970abd40061cad702b0c4629
y-aut <y_autumn0901@icloud.com>
2024-04-25 15:07:03 +0900

    add submodule Luzhiled's Library

--- src/abc/350/g3.cpp
+++ src/abc/350/g3.cpp
@@ -214,63 +214,246 @@ int main() {
 
 DEFINE_MOD(MOD);
 
+/**
+ * @brief Link Cut Tree
+ *
+ */
+template <typename T, typename F, typename S>
+struct LinkCutTree {
+
+private:
+    F f;
+    S s;
+
+    struct Node {
+        Node *l, *r, *p;
+        T key, sum;
+        bool rev;
+        size_t sz;
+
+        explicit Node(const T &v) : key(v), sum(v), sz(1), rev(false),
+                                    l(nullptr), r(nullptr), p(nullptr) {}
+
+        bool is_root() const {
+            return not p or (p->l != this and p->r != this);
+        }
+    };
+
+public:
+    using NP = Node *;
+
+private:
+    NP update(NP t) {
+        t->sz = 1;
+        t->sum = t->key;
+        if (t->l) t->sz += t->l->sz, t->sum = f(t->l->sum, t->sum);
+        if (t->r) t->sz += t->r->sz, t->sum = f(t->sum, t->r->sum);
+        return t;
+    }
+
+    void rotr(NP t) {
+        NP x = t->p, y = x->p;
+        if ((x->l = t->r)) t->r->p = x;
+        t->r = x, x->p = t;
+        update(x), update(t);
+        if ((t->p = y)) {
+            if (y->l == x) y->l = t;
+            if (y->r == x) y->r = t;
+            update(y);
+        }
+    }
+
+    void rotl(NP t) {
+        NP x = t->p, y = x->p;
+        if ((x->r = t->l)) t->l->p = x;
+        t->l = x, x->p = t;
+        update(x), update(t);
+        if ((t->p = y)) {
+            if (y->l == x) y->l = t;
+            if (y->r == x) y->r = t;
+            update(y);
+        }
+    }
+
+    void toggle(NP t) {
+        swap(t->l, t->r);
+        t->sum = s(t->sum);
+        t->rev ^= true;
+    }
+
+    void push(NP t) {
+        if (t->rev) {
+            if (t->l) toggle(t->l);
+            if (t->r) toggle(t->r);
+            t->rev = false;
+        }
+    }
+
+    void splay(NP t) {
+        push(t);
+        while (not t->is_root()) {
+            NP q = t->p;
+            if (q->is_root()) {
+                push(q), push(t);
+                if (q->l == t) rotr(t);
+                else rotl(t);
+            } else {
+                NP r = q->p;
+                push(r), push(q), push(t);
+                if (r->l == q) {
+                    if (q->l == t) rotr(q), rotr(t);
+                    else rotl(t), rotr(t);
+                } else {
+                    if (q->r == t) rotl(q), rotl(t);
+                    else rotr(t), rotl(t);
+                }
+            }
+        }
+    }
+
+public:
+    LinkCutTree(const F &f, const S &s) : f(f), s(s) {}
+
+    NP alloc(const T &v = T()) {
+        return new Node(v);
+    }
+
+    vector<NP> build(vector<T> &vs) {
+        vector<NP> nodes(vs.size());
+        for (int i = 0; i < (int)vs.size(); i++) {
+            nodes[i] = alloc(vs[i]);
+        }
+        return nodes;
+    }
+
+    NP expose(NP t) {
+        NP rp = nullptr;
+        for (NP cur = t; cur; cur = cur->p) {
+            splay(cur);
+            cur->r = rp;
+            update(cur);
+            rp = cur;
+        }
+        splay(t);
+        return rp;
+    }
+
+    void evert(NP t) {
+        expose(t);
+        toggle(t);
+        push(t);
+    }
+
+    void link(NP child, NP parent) {
+        if (is_connected(child, parent)) {
+            throw runtime_error("child and parent must be different connected components");
+        }
+        if (child->l) {
+            throw runtime_error("child must be root");
+        }
+        child->p = parent;
+        parent->r = child;
+        update(parent);
+    }
+
+    void cut(NP child) {
+        expose(child);
+        NP parent = child->l;
+        if (not parent) {
+            throw runtime_error("child must not be root");
+        }
+        child->l = nullptr;
+        parent->p = nullptr;
+        update(child);
+    }
+
+    bool is_connected(NP u, NP v) {
+        expose(u), expose(v);
+        return u == v or u->p;
+    }
+
+    NP lca(NP u, NP v) {
+        if (not is_connected(u, v)) return nullptr;
+        expose(u);
+        return expose(v);
+    }
+
+    NP get_kth(NP x, int k) {
+        expose(x);
+        while (x) {
+            push(x);
+            if (x->r && x->r->sz > k) {
+                x = x->r;
+            } else {
+                if (x->r) k -= x->r->sz;
+                if (k == 0) {
+                    splay(x);
+                    return x;
+                }
+                k -= 1;
+                x = x->l;
+            }
+        }
+        return nullptr;
+    }
+
+    const T &query(NP u) {
+        expose(u);
+        return u->sum;
+    }
+
+    const T &query(NP u, NP v) {
+        evert(u);
+        return query(v);
+    }
+
+    void set_key(NP t, T v) {
+        expose(t);
+        t->key = v;
+        update(t);
+    }
+};
+
+template <typename T, typename F, typename S>
+LinkCutTree<T, F, S> get_link_cut_tree(const F &f, const S &s) {
+    return {f, s};
+}
+
 void solve() {
-    LL(N);
-    VLL(A, N);
+    LL(N, Q);
 
-    map<ll, vll> m;
-    vll bf(N, -1);
-    rep(i, N) {
-        auto upper = m.upper_bound(A[i]);
-        auto itr = upper;
-        while (itr != m.begin()) {
-            itr--;
-            repi(j, itr->second) bf[j] = i;
-        }
-        m.erase(m.begin(), upper);
-        m[A[i]].pb(i);
-    }
-    m.clear();
-    vll bb(N, -1);
-    repd(i, N) {
-        auto upper = m.upper_bound(A[i]);
-        auto itr = upper;
-        while (itr != m.begin()) {
-            itr--;
-            repi(j, itr->second) bb[j] = i;
-        }
-        m.erase(m.begin(), upper);
-        m[A[i]].pb(i);
-    }
+    ll X = 0;
 
-    vll rmax(N), lmax(N);
-    ll max_v = -LINF;
-    rep(i, N) {
-        if (A[i] >= max_v) {
-            rmax[i] = i;
-            max_v = A[i];
+    auto f = [](ll a, ll b) { return a + b; };
+    auto s = [](ll v) { return v; };
+
+    auto lct = get_link_cut_tree<ll>(f, s);
+    vll nums(N);
+    rep(i, N) nums[i] = i;
+    auto nodes = lct.build(nums);
+
+    rep(q, Q) {
+        LL(a, b, c);
+        ll A = ((a * (1 + X)) % MOD2) % 2;
+        ll u = ((b * (1 + X)) % MOD2) % N;
+        ll v = ((c * (1 + X)) % MOD2) % N;
+        if (A == 0) {
+            lct.evert(nodes[u]);
+            lct.link(nodes[u], nodes[v]);
         } else {
-            rmax[i] = rmax[i - 1];
+            ll ans = -1;
+            if (lct.is_connected(nodes[u], nodes[v])) {
+                auto up = lct.get_kth(nodes[u], 1);
+                auto vp = lct.get_kth(nodes[v], 1);
+                auto upp = lct.get_kth(nodes[u], 2);
+                auto vpp = lct.get_kth(nodes[v], 2);
+                if (up == vp) ans = up->key;
+                else if (up != nullptr && upp == nodes[v]) ans = up->key;
+                else if (vp != nullptr && vpp == nodes[u]) ans = vp->key;
+            }
+            ans++;
+            print(ans);
+            X = ans;
         }
     }
-    max_v = -LINF;
-    repd(i, N) {
-        if (A[i] >= max_v) {
-            lmax[i] = i;
-            max_v = A[i];
-        } else {
-            lmax[i] = lmax[i + 1];
-        }
-    }
-
-    ll ans = 0;
-    rep(i, 1, N - 1) {
-        ll l, r;
-        if (bb[i] != -1) l = bb[i];
-        else l = rmax[i - 1];
-        if (bf[i] != -1) r = bf[i];
-        else r = lmax[i + 1];
-        chmax(ans, r - l);
-    }
-    print(ans);
 }
--- src/work/google/naive.cpp
+++ src/work/google/naive.cpp
@@ -218,58 +218,31 @@ void solve() {
     LL(N);
     VLL(A, N);
 
-    map<ll, vll> m;
-    vll bf(N, -1);
-    rep(i, N) {
-        auto upper = m.upper_bound(A[i]);
-        auto itr = upper;
-        while (itr != m.begin()) {
-            itr--;
-            repi(j, itr->second) bf[j] = i;
-        }
-        m.erase(m.begin(), upper);
-        m[A[i]].pb(i);
-    }
-    m.clear();
-    vll bb(N, -1);
-    repd(i, N) {
-        auto upper = m.upper_bound(A[i]);
-        auto itr = upper;
-        while (itr != m.begin()) {
-            itr--;
-            repi(j, itr->second) bb[j] = i;
-        }
-        m.erase(m.begin(), upper);
-        m[A[i]].pb(i);
-    }
-
-    vll rmax(N), lmax(N);
-    ll max_v = -LINF;
-    rep(i, N) {
-        if (A[i] >= max_v) {
-            rmax[i] = i;
-            max_v = A[i];
-        } else {
-            rmax[i] = rmax[i - 1];
-        }
-    }
-    max_v = -LINF;
-    repd(i, N) {
-        if (A[i] >= max_v) {
-            lmax[i] = i;
-            max_v = A[i];
-        } else {
-            lmax[i] = lmax[i + 1];
-        }
-    }
-
     ll ans = 0;
     rep(i, 1, N - 1) {
-        ll l, r;
-        if (bb[i] != -1) l = bb[i];
-        else l = rmax[i - 1];
-        if (bf[i] != -1) r = bf[i];
-        else r = lmax[i + 1];
+        ll l = 0, r = 0;
+        ll v = -LINF;
+        rep(j, i + 1, N) {
+            if (A[j] >= A[i]) {
+                r = j;
+                break;
+            }
+            if (A[j] > v) {
+                v = A[j];
+                r = j;
+            }
+        }
+        v = -LINF;
+        repd(j, 0, i) {
+            if (A[j] >= A[i]) {
+                l = j;
+                break;
+            }
+            if (A[j] > v) {
+                v = A[j];
+                l = j;
+            }
+        }
         chmax(ans, r - l);
     }
     print(ans);
